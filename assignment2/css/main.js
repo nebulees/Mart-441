bba9fcd60b8ef37b573a8eb44c406c04480745fb
Raelynn Klein <112666354+nebulees@users.noreply.github.com>
2023-01-30 02:02:49 -0500

    Add files via upload

---
+++ assignment2/css/main.js
@@ -0,0 +1,23 @@
+/*
+Websites I enjoy: 
+- Youtube 
+- Tumblr 
+- Discord 
+*/
+
+/*
+Games I enjoy: 
+- Destiny 2  
+- Hollow Knight
+- Pyre (SuperGiant Games)
+*/
+
+/*
+Artists I enjoy: 
+I don't have any favorite artists that affect the culture, but here's links to artists that are close to me, sister, my wife, and a friend in that order. 
+- Wendy Red Star - She's spent her career upending stoic portrayals of Native Americans in photographs, sculptures, videos, fiber arts, and performances.
+- Carla Jay Harris - Carla Jay Harris is a multidisciplinary artist whose practice includes photography, installation, collage, drawing, and digital image making. Her work documents intellectual, emotional and psychological environments.
+- Lizzo, a musical artist that advocates for body positivity, and just happens to also make amazing songs. 
+*/
+
+console.log("JavaScript Rules!");
